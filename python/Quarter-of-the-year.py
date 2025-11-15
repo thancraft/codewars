3b3603abdeebf200ac45755af9e7a071b7f48f66
Ahmad Fadli <thancraft313@gmail.com>
2025-11-15 20:01:43 +0900

    adding file

---
+++ python/Quarter-of-the-year.py
@@ -0,0 +1,14 @@
+# import math
+
+# def quarter_of(month):
+#     return math.ceil(month/3)
+
+def quarter_of(month):
+    if month in range(1, 4):
+        return 1
+    if month in range(4, 7):
+        return 2
+    if month in range(7, 10):
+        return 3
+    if month in range(10, 13):
+        return 4
